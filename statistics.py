b48e3b76a5c359eecc57e824a82e35d24f6dc253
Vapourism233 <127520189+Vapourism233@users.noreply.github.com>
2023-09-09 21:11:13 +0900

    Add files via upload

--- statistics.py
+++ statistics.py
@@ -2,24 +2,18 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('/Users/apple/Desktop/BoschIntern/DisparityPattern/disparity.png', 0)
 
-width, height = img.shape
-max_pixel = 255
-img_save = np.zeros((width, height), dtype=np.float32)
+def statictics():
+    sum = 0
+    color = ('blue', 'green', 'red')
+    img = cv2.imread('/Users/apple/Desktop/BoschIntern/DisparityPattern/disparity.png')
 
-for y in range(width):
-    for x in range(height):
-        pixel = img[y, x]
-        percentage = (pixel / max_pixel) * 100
-        img_save[y, x] = percentage
-
-histogram, bin_edge = np.histogram(img_save, bins=101, range=(0, 100))
-
-plt.figure(figsize=(10, 6))
-plt.xlim(0, 255)
-
-plt.ylim(0, 10000)
-plt.bar(bin_edge[:-1], height, width=1, align="edge")
-plt.show()
-
+    for i, color in enumerate(color):
+        hist = cv2.calcHist([img], [i], None, [256], [0, 256])
+    for i in range(len(hist)):
+        sum += hist[i]
+    for i in range(len(hist)):
+        hist[i] = hist[i] / sum
+    plt.plot(hist, color=color)
+    plt.xlim([0, 256])
+    plt.show()
